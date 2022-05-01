0c15609e517bb065673641d6e450452dc18ebaa8
Severin <insmer@gmail.com>
2022-05-02 02:10:29 +0500

    first commit for task1

---
+++ src/com/javalevelbasic/task001/ReadNumbers.java
@@ -0,0 +1,13 @@
+package com.javalevelbasic.task001;
+
+/**
+ * Write a program that reads a 5-digit number from the
+ * keyboard and outputs the digits of which it consists.
+ * For example : The number 54698 is read
+ */
+public class ReadNumbers {
+
+    public static void main(String[] args) {
+        ;
+    }
+}
