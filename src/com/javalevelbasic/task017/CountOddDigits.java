62aecb2273f10da4d795736f025626a493169f61
Severin <insmer@gmail.com>
2022-05-08 12:30:34 +0500

    solving the problem 017

---
+++ src/com/javalevelbasic/task017/CountOddDigits.java
@@ -0,0 +1,24 @@
+package com.javalevelbasic.task017;
+
+/**
+ * Given an array — write a program to count odd digits in it.
+ */
+public class CountOddDigits {
+
+    public static final int[] ARRAY = {0, 5, 2, 4, 7, 1, 3, 19};
+
+    public static void main(String[] args) {
+        System.out.println(countOddDigits(ARRAY));
+    }
+
+    private static int countOddDigits(int[] array) {
+        int count = 0;
+
+        for (int i : array) {
+            if (i % 2 == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
